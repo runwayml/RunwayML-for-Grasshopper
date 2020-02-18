c6e80144de6ce193b6b2f330363a526d67ba7ec4
vahid <vahid.azizi.1992@gmail.com>
2020-02-18 23:52:04 +0330

    update to version 1.0.0.1
    this is first version

---
+++ component/DecomposeImage.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Grasshopper.Kernel;
+using Rhino.Display;
+using Rhino.Geometry;
+using Rhino.Geometry.MeshRefinements;
+
+namespace Runway
+{
+    public class DecomposeImage : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the showimage class.
+        /// </summary>
+        public DecomposeImage()
+          : base("Decompose Image", "di",
+              "Convert data  output to image ",
+              "Runway",
+              "Image")
+        {
+        }
+        
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Input Runway Data", "IRD", "input runway Data from Runway component ", GH_ParamAccess.item);
+            pManager.AddBooleanParameter( "Run ","R" ," Run convert base 64 to image  ", GH_ParamAccess.item,false);
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Red", "R", "Red channel", GH_ParamAccess.item);
+            pManager.AddTextParameter("Green", "G", "Green channel", GH_ParamAccess.item);
+            pManager.AddTextParameter("Blue", "B", "Blue channel", GH_ParamAccess.item);
+            pManager.AddTextParameter("Alpha", "A", "Alpha channel", GH_ParamAccess.item);
+            pManager.AddTextParameter("Width", "W", "Width channel", GH_ParamAccess.item);
+            pManager.AddTextParameter("Height", "H", "Height channel", GH_ParamAccess.item);
+
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // define input
+            String setdata = "";
+            Boolean Reset = false;
+
+            int pixwidth = 0;
+            int pixheight = 0;
+
+            List<int> red = new List<int>();
+            List<int> green = new List<int>();
+            List<int> blue = new List<int>();
+            List<int> alpha = new List<int>();
+
+            //define input grasshopper component to var
+
+            DA.GetData(0, ref setdata);
+            DA.GetData(1, ref Reset);
+
+            // convert data
+
+            if (Reset)
+            {
+                
+                //filter json data to base 64 image 
+                char[] delimiterChars = {'"', '{', '}', ','};
+                string phrase = setdata;
+                string[] outputData = phrase.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+                string finaltext = outputData[3];
+                //convert base 64 to image
+
+                Byte[] bitmapData = Convert.FromBase64String(finaltext);
+                System.IO.MemoryStream streamBitmap = new System.IO.MemoryStream(bitmapData);
+                Bitmap bitImage = new Bitmap((Bitmap) Image.FromStream(streamBitmap));
+
+                // width and height image ref to out put
+                 pixwidth = bitImage.Width;
+                 pixheight = bitImage.Height;
+
+
+                // decompose image to rgb 
+
+
+                using (Bitmap bmp = new Bitmap(bitImage))
+                {
+                    for (int i = 0; i < bitImage.Width; i++)
+                    {
+                        for (int j = 0; j < bitImage.Height; j++)
+                        {
+                            Color clr = bmp.GetPixel(i, j);
+                            int redget = clr.R;
+                            red.Add(redget);
+                            int greenget = clr.G;
+                            green.Add(greenget);
+                            int blueget = clr.B;
+                            blue.Add(blueget);
+                            alpha.Add(255);
+
+                        }
+
+                    }
+
+
+                }
+            }
+
+            
+
+
+            //set data to out put component
+
+
+
+            DA.SetDataList(0, red);
+            DA.SetDataList(1, green);
+            DA.SetDataList(2, blue);
+            DA.SetDataList(3, alpha);
+            DA.SetData(4, pixwidth);
+            DA.SetData(5, pixheight);
+
+
+
+
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return Properties.Resources.Group_26;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("3750da58-fee7-4a96-8791-04929e85648e"); }
+        }
+    }
+}
---
+++ component/filterData.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System.Text.RegularExpressions;
+using System.Linq;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel.Attributes;
+
+namespace Runway
+{
+    public class FilterDataComponent_custom : GH_ComponentAttributes
+    {
+        public FilterDataComponent_custom(IGH_Component component) : base(component) { }
+
+        protected override void Render(GH_Canvas canvas, Graphics graphics, GH_CanvasChannel channel)
+        {
+            GH_PaletteStyle styleStandard = null;
+            GH_PaletteStyle styleSelected = null;
+
+            
+            
+
+            if (channel == GH_CanvasChannel.Objects)
+            {
+                styleStandard = GH_Skin.palette_normal_standard;
+                styleSelected = GH_Skin.palette_normal_selected;
+
+                GH_Skin.palette_normal_standard = new GH_PaletteStyle(Color.Black, Color.LightSalmon, Color.DarkSlateGray);
+                GH_Skin.palette_normal_selected = new GH_PaletteStyle(Color.SkyBlue, Color.DarkBlue, Color.Black);
+                // Restore the cached styles.
+                base.Render(canvas, graphics, channel);
+                GH_Skin.palette_normal_standard = styleStandard;
+                GH_Skin.palette_normal_selected = styleSelected;
+              
+
+            }
+           else
+            {
+
+                base.Render(canvas, graphics, channel);
+
+            }
+
+
+        }
+    }
+
+    public class FilterDataComponent : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public FilterDataComponent()
+          : base(
+              "Filter Data",
+              "FD",
+              "filter Data by input value",
+              "Runway",
+              "Filters")
+        {
+        }
+
+        public override void CreateAttributes()
+        {
+            base.m_attributes = new FilterDataComponent_custom(this);
+        }
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Input Data", "I", "Input data from Receive ", GH_ParamAccess.item);
+
+        }
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.tertiary; }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("number in model", "nm", "float data receive", GH_ParamAccess.list);
+            pManager.AddTextParameter("name category", "ng", "name category from data", GH_ParamAccess.list);
+            pManager.AddTextParameter("output data", "od", "receive all data from model in runway", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string inputData = "";
+            DA.GetData(0, ref inputData);
+            //filter data
+            char[] delimiterChars = { ',', '[', ']', '\"', '{', '}', ':' };
+            string phrase = inputData;
+            string[] outputData = phrase.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+
+
+            //filter text
+            List<string> outputText = new List<string>();
+
+            string[] textRegex = Regex.Split(inputData, "[^a-zA-Z]+");
+
+            foreach (var i in textRegex)
+            {
+                outputText.Add(i);
+            }
+
+            outputText = outputText.Where(x => !string.IsNullOrEmpty(x)).ToList();
+            //filter number
+            List<string> outputNum = new List<string>();
+            string[] numRexex = Regex.Split(inputData, @"[^\.0-9]+");
+
+            foreach (var numi in numRexex)
+            {
+                outputNum.Add(numi);
+            }
+            outputNum = outputNum.Where(x => !string.IsNullOrEmpty(x)).ToList();
+
+
+            //set data;
+
+            DA.SetDataList(0, outputNum);
+            DA.SetDataList(1, outputText);
+            DA.SetDataList(2, outputData);
+
+
+
+
+
+
+        }
+
+        /// <summary>
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return Properties.Resources.Group_28; 
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("8be70209-fca8-41ca-9952-ac018d7a7082"); }
+        }
+    }
+}
